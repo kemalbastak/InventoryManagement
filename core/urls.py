a005ab578cde936cfb48359fdd76232a6dfaef8e
Kemal Cihan Bastak <bastakkemal@gmail.com>
2021-10-25 20:16:46 +0300

    email sender forgot password

--- core/urls.py
+++ core/urls.py
@@ -27,4 +27,8 @@ urlpatterns = [
     path('', include('user.urls')),
     path('',auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    path('password_reset/',auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
